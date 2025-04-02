56afe8a92fb22a7f3466de59839352f394510d3b
Cocodrulo <142546774+Cocodrulo@users.noreply.github.com>
2025-04-02 12:06:16 +0100

    Enhance fuel synchronization by adding state change handlers and event listeners for vehicle fuel updates

--- client/exports.lua
+++ client/exports.lua
@@ -5,20 +5,62 @@ local newExports = function (funcName, func)
     exports(funcName, func)
 end
 
-SetFuel = function (vehicle, fuel)
-    if not DoesEntityExist(vehicle) then return end
-    if not IsEntityAVehicle(vehicle) then return end
+local SetFuel = function (vehicle, fuel)
+    if not DoesEntityExist(vehicle) or not IsEntityAVehicle(vehicle) then return end
     if fuel < 0 then fuel = 0 end
     if fuel > 100 then fuel = 100 end
+
+    NetworkRequestControlOfEntity(vehicle)
     SetVehicleFuelLevel(vehicle, fuel + 0.0)
+    if Config.SyncFuelBetweenPlayers then Entity(vehicle).state:set('qb-fuel', fuel + 0.0, true) end
 end
 
 newExports('SetFuel', SetFuel)
 
-GetFuel = function (vehicle)
-    if not DoesEntityExist(vehicle) then return end
-    if not IsEntityAVehicle(vehicle) then return end
+local GetFuel = function (vehicle)
+    if not DoesEntityExist(vehicle) or not IsEntityAVehicle(vehicle) then return end
+
     return GetVehicleFuelLevel(vehicle)
 end
 
 newExports('GetFuel', GetFuel)
+
+if Config.SyncFuelBetweenPlayers then
+
+    AddStateBagChangeHandler("qb-fuel", nil, function(bagName, _, value)
+        local entity = GetEntityFromStateBagName(bagName)
+        if entity == 0 then return end
+        if not DoesEntityExist(entity) or not IsEntityAVehicle(entity) then return end
+        SetVehicleFuelLevel(entity, value + 0.0)
+    end)
+
+    AddEventHandler("gameEventTriggered", function(event, data)
+        if event ~= "CEventNetworkPlayerEnteredVehicle" then return end
+        local player, vehicle = data[1], data[2]
+        local playerPed = PlayerPedId()
+        if player ~= 128 or GetPedInVehicleSeat(vehicle, -1) ~= playerPed then return end
+
+        while DoesEntityExist(vehicle) and GetPedInVehicleSeat(vehicle, -1) == playerPed do
+            if IsVehicleEngineOn(vehicle) then
+                Entity(vehicle).state:set('qb-fuel', GetVehicleFuelLevel(vehicle) + 0.0, true)
+            end
+            Wait(10000)
+        end
+    end)
+
+    AddEventHandler('onResourceStart', function(resourceName)
+        if resourceName ~= GetCurrentResourceName() then return end
+
+        local playerPed = PlayerPedId()
+        local vehicle = GetVehiclePedIsIn(playerPed, false)
+        if vehicle == 0 or GetPedInVehicleSeat(vehicle, -1) ~= playerPed then return end
+
+        while DoesEntityExist(vehicle) and GetPedInVehicleSeat(vehicle, -1) == playerPed do
+            if IsVehicleEngineOn(vehicle) then
+                local fuel = GetVehicleFuelLevel(vehicle)
+                Entity(vehicle).state:set('qb-fuel', fuel + 0.0, true)
+            end
+            Wait(10000)
+        end
+    end)
+end
